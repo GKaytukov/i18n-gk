2f4125459853cf594edc073f49c66c155d7f555b
Gerly Kaytukov <gerlyecc@yahoo.com>
2022-11-15 14:44:44 -0500

    updating code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import { useState } from 'react'
+import { createContext, useState } from 'react'
+import Details from './components/Details'
 import English from './lang/en.json'
 import Spanish from './lang/es.json'
+import French from './lang/fr.json'
 import './App.css';
 
 const locale = navigator.language || 'en'; 
 
-const _lang = (locale==='es') ? Spanish : English; 
+const _lang = (locale==='es') 
+              ? Spanish 
+              : (locale==='fr')
+              ? French
+              : English; 
+
+export const LanguageContext = createContext()
 
 function App() {
   const [lang, setLang] = useState(_lang)
   return (
+    <LanguageContext.Provider value={{ lang }}> 
     <div className="App">
       <header className="App-header">
         <div>
           <button onClick={() => setLang(English)}>EN</button>
           <button onClick={() => setLang(Spanish)}>ES</button>
+          <button onClick={() => setLang(French)}>FR</button>
+
         </div>
         <h1>{lang["app.header"]}</h1>
         <p>{lang["app.subhead"]}</p>
@@ -24,10 +35,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {lang["app.cta"]}
         </a>
+      <Details />
       </header>
     </div>
+    </LanguageContext.Provider>
   );
 }
 
